90642e089d69d3a4d741a258a3bd344f8220ee2f
Mario Ribeiro <mario.ribas@gmail.com>
2013-12-27 16:39:58 +0000

    update

--- src/sys/qk_system.h
+++ src/sys/qk_system.h
@@ -41,6 +41,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 /*****************************************************************************
  *      HAL (Hardware Abstraction Layer)
