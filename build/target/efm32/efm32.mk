126aa47ee7e9ed170dae3001f9e2aeacc6e84d51
Mario Ribeiro <mario.ribas@gmail.com>
2014-04-23 04:44:57 +0100

    update

--- build/target/efm32/efm32.mk
+++ build/target/efm32/efm32.mk
@@ -9,17 +9,19 @@ include $(TARGET_GLOBAL)
 ###############################################################################
 INCLUDE_DIR += \
 $(QKPROGRAM_DIR)/lib/hal/efm32 \
-$(ENERGYMICRO)/CMSIS/Include \
-$(ENERGYMICRO)/Device/EnergyMicro/EFM32G/Include \
 $(ENERGYMICRO)/emlib/inc
+
+#$(ENERGYMICRO)/CMSIS/Include 
+#$(ENERGYMICRO)/Device/EnergyMicro/EFM32G/Include 
 
 C_SRC_DIR += \
 $(QKPROGRAM_DIR)/lib/hal/efm32
 
-#$(ENERGYMICRO)/emlib/src/em_ebi.c \
+#$(ENERGYMICRO)/emlib/src/em_ebi.c 
+
+#$(ENERGYMICRO)/Device/EnergyMicro/EFM32G/Source/system_efm32g.c 
 
 C_SRC += \
-$(ENERGYMICRO)/Device/EnergyMicro/EFM32G/Source/system_efm32g.c \
 $(ENERGYMICRO)/emlib/src/em_assert.c \
 $(ENERGYMICRO)/emlib/src/em_system.c \
 $(ENERGYMICRO)/emlib/src/em_cmu.c \
@@ -28,5 +30,5 @@ $(ENERGYMICRO)/emlib/src/em_timer.c \
 $(ENERGYMICRO)/emlib/src/em_gpio.c \
 $(ENERGYMICRO)/emlib/src/em_usart.c
 
-S_SRC += $(ENERGYMICRO)/Device/EnergyMicro/EFM32G/Source/GCC/startup_efm32g.S
+#S_SRC += $(ENERGYMICRO)/Device/EnergyMicro/EFM32G/Source/GCC/startup_efm32g.S
 
